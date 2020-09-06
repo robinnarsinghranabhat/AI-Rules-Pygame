57db6bee573e56fd0aaac392ac101f5f0408e243
whatsGr8t <robinnarsingha123@gmail.com>
2020-09-06 13:38:18 +0545

    [gymlike] RL AGENT ADDED

--- flap/agent_play.py
+++ flap/agent_play.py
@@ -8,8 +8,13 @@ import random
 import time
 # state = env.get_screen_rbg()
 ## play for 200 frames
-for i in range(2000):
-    action = random.choice([0,1,2])
-    next_state, reward, active = env.step( action )
-    if not active:
-        break
+while True:
+    print('Starting New game ...')
+    tot_rew = 0
+    for i in range(2000):
+        action = random.choice([0,1,2])
+        next_state, reward, active = env.step( action )
+        tot_rew += reward if reward is not None else 0
+        if not active:
+            print('Good reward : ', tot_rew )
+            break
--- flap/main_game_class.py
+++ flap/main_game_class.py
@@ -3,45 +3,56 @@
 ### Check for event in while loop, and modify/ draw object's internal state ..
 
 import pygame, sys
+import numpy as np
+
 from flappy_sprite_utils import Flappy, Floor, check_collision, update_score, Pipe
-
-## MAIN CODE
-pygame.init()
-screen_width = 576
-screen_height = 1024
-
-clock = pygame.time.Clock()
-game_font = pygame.font.Font('04B_19.TTF',40)
-gravity = 0.3
-game_active = True
-score = 0
-high_score = 0
-
-game_over_surface = pygame.transform.scale2x(pygame.image.load('assets/message.png').convert_alpha())
-game_over_rect = game_over_surface.get_rect(center = (288,512))
-
 
 ## MAIN GAME CLASS TO GET UPDATED FRAMES EVERY SECOND, ALONG WITH REWARDS
 class Flappy_Main(object):
 
-    def __init__( self , screen_width , screen_height ):
+    def __init__( self , screen_width = 576 , screen_height = 1024 ):
 
-        self.screen = pygame.display.set_mode((  screen_width  ,screen_height))
-        self.all_sprites = pygame.sprite.OrderedUpdates()
         self.screen_width = screen_width
         self.screen_height = screen_height
 
+        ## load immutable game variables ##
+        self.gravity = 0.3
+        self.fps = 60
+        self.action_dict = {}
+        self.display_screen = True
+
+        ## load mutable game variables ##
+        self.init()
+        self._start()
+
+    ## Don't make pygame object till we explicitely call init() ##
+    def _start(self):
+        ## LOAD THE MUTABLE GRAPHICS ##
+        self.all_sprites = pygame.sprite.OrderedUpdates()
         self._init_bird()
         self._init_pipes()
         self._init_floor()
         self._load_bg()
+
+        self.frame_count = 0
+        self.time_elapsed = 0
+
+    ## Load it when you make a main game object ##
+    def init(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((  self.screen_width  , self.screen_height ))
+        self.clock = pygame.time.Clock()
+
+
+    def _reset(self):
+        self._start()
+        self.init()
 
     def _init_bird(self):
         self.bird = Flappy()
         self.BIRDFLAP = pygame.USEREVENT + 1
         pygame.time.set_timer( self.BIRDFLAP , 200 )
         self.all_sprites.add( self.bird )
-
 
     def _init_pipes(self):
         self.game_pipes = pygame.sprite.Group( [
@@ -54,19 +65,59 @@ class Flappy_Main(object):
             self.all_sprites.add(i)
 
     def _init_floor(self):
-        self.floor = Floor(screen_width, screen_height)
-        self.SPAWNPIPE = pygame.USEREVENT
-        pygame.time.set_timer( self.SPAWNPIPE , 1200 )
+        self.floor = Floor(self.screen_width, self.screen_height)
         self.all_sprites.add( self.floor)
-
 
     def _load_bg(self):
         bg_surface = pygame.image.load('assets/background-day.png').convert()
         self.bg_surface = pygame.transform.scale2x(bg_surface)
         self.screen.blit( self.bg_surface , (0,0) )
 
+    def _handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            if event.type == self.BIRDFLAP:
+                if self.bird.bird_index < 2:
+                    self.bird.bird_index += 1
+                else:
+                    self.bird.bird_index = 0
+                # self.bird.bird_animation()
+
+
+
+    def step(self, action):
+        ## 0 -> up , 1 -> down , 2 -> nothing
+        # self.screen.unlock()
+        if action == 0 :
+            self.bird.update( - 10 , pygame.K_UP )
+        elif action == 1 :
+            self.bird.update( 10 , pygame.K_DOWN )
+
+        self.clock.tick(self.fps)
+        # APART FROM THESE ACTIONS< NOTHING MODIFYING THE BIRD !!!
+
+        game_active = check_collision( self.game_pipes , self.bird )
+
+        if game_active:
+            self.frame_count += 1
+            self.game_pipes.update()
+            self.floor.update()
+            self.update_screen()
+
+            # if self.display_screen:
+            #     pygame.display.update()
+            return self.get_screen_rbg() , self.frame_count , True
+
+        else:
+            self._reset()
+            return None, None, False
 
     def update_screen(self):
+
+        self.screen.blit( self.bg_surface , (0,0) )
         for entity in self.all_sprites:
             if entity.__str__() == 'Floor':
                 self.screen.blit( entity.image,  entity.rect1  )
@@ -74,94 +125,15 @@ class Flappy_Main(object):
 
             elif entity.__str__() == 'Pipe':
                 self.screen.blit( entity.lower_pipe ,  entity.lower_pipe_rect  )
-                screen.blit( entity.upper_pipe ,  entity.upper_pipe_rect  )
+                self.screen.blit( entity.upper_pipe ,  entity.upper_pipe_rect  )
 
             else:
                 self.screen.blit( entity.image, entity.rect )
+        if self.display_screen:
+            pygame.display.flip()
 
-
-
-
-
-
-    def _load_game_over(self):
-
-
-
-
-
-
-
-
-
-
-
-
-
-floor = Floor(screen_width, screen_height)
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP and game_active:
-                bird.update(-10, event.key)
-                flap_sound.play()
-
-            if event.key == pygame.K_DOWN and game_active:
-                bird.update(10, event.key)
-                flap_sound.play()
-
-            if event.key == pygame.K_UP and game_active == False:
-                game_active = True
-                for ind, pipe in enumerate(game_pipes):
-                    pipe.reinitialize(ind)
-                bird.rect.center = (100,512)
-                score = 0
-                flap_sound.play()
-
-        if event.type == BIRDFLAP:
-            if bird.bird_index < 2:
-                bird.bird_index += 1
-            else:
-                bird.bird_index = 0
-            bird.bird_animation()
-
-    screen.blit(bg_surface,(0,0))
-    if game_active:
-        bird.update(gravity)
-        game_active = check_collision( game_pipes , bird )
-
-        game_pipes.update()
-        floor.update()
-
-
-        for entity in all_sprites:
-            if entity.__str__() == 'Pipe':
-                screen.blit( entity.lower_pipe ,  entity.lower_pipe_rect  )
-                screen.blit( entity.upper_pipe ,  entity.upper_pipe_rect  )
-            else:
-                screen.blit(entity.image, entity.rect)
-        floor.draw(screen)
-        ## DISPLAY PART END ##
-        try:
-            score += 0.01
-            score_display('main_game')
-            score_sound_countdown -= 1
-            if score_sound_countdown <= 0:
-                score_sound.play()
-                score_sound_countdown = 100
-        except:
-            import pdb; pdb.set_trace()
-    else:
-        screen.blit(game_over_surface,game_over_rect)
-        high_score = update_score(score,high_score)
-        score_display('game_over')
-
-
-    pygame.display.update()
-    # new_array = pygame.surfarray.pixels3d(screen)
-
-    clock.tick(60)
+    def get_screen_rbg(self):
+        # return pygame.surfarray.pixels3d(self.screen)
+        ## same thing as this
+        out =  pygame.surfarray.array3d( pygame.display.get_surface()).astype(np.uint8)
+        return out
